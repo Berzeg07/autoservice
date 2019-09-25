7e2b18c9601da8c56b36eda83881533b5274485e
Aslan <berezg86@gmail.com>
2019-09-25 17:06:15 +0300

    3 блока на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     });
 
     // mobile menu *
-    $('.burger').click(function(){
+    $('.burger').click(function() {
         $(this).toggleClass('active');
         $('.header-mobile__hidden').slideToggle();
         $('body').toggleClass('hidden');
@@ -22,5 +22,57 @@ $(document).ready(function() {
 
     // custom select *
     $('.select-custom select').selectmenu();
+
+    // car model slider *
+    var modelSlider = $(".model-slider");
+    modelSlider.owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        center: false,
+        navText: ['<span class="nav-left"></span>', '<span class="nav-right"></span>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            375: {
+                items: 2
+            },
+            768: {
+                items: 5
+            },
+            992: {
+                items: 6
+            },
+            1280: {
+                items: 7
+            },
+            1400: {
+                items: 9
+            }
+        }
+    });
+
+    // advantage slider *
+    var advSlider = $(".advantages-slider");
+    advSlider.owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        center: false,
+        navText: ['<span class="nav-left"></span>', '<span class="nav-right"></span>'],
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    });
 
 });
